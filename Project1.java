67172b13f161d4e82e40389200eabc6c001590ac
PEDRO MANUEL <pm@PEDROs-Laptop.local>
2024-09-15 23:05:19 -0600

    Algoritmo Pancake sort agregado junto a la generación de datos

--- Project1.java
+++ Project1.java
@@ -1,16 +1,109 @@
-package Sort;
+import java.util.Random;
+import Sort.BubbleSort;
+import Sort.HeapSort; 
+import Sort.InsertionSort; 
+import Sort.QuickSort; 
+import Sort.SelectionSort; 
+import Sort.MergeSort;
+import Sort.PancakeSort;
+import Sort.Utilerias; 
+import java.util.ArrayList;
 
 
 public class Project1 {
     public static void main(String[] args) {
-        int size = 13, cont1, cont2, cont3 ,cont4 ,cont5, cont6; 
+        Random random = new Random(); 
+
+    //Generación de números aleatorios
+        int[] sizes = {50, 100, 500, 800, 1000, 2000, 5000, 10000};
+        ArrayList<ArrayList<ArrayList<Integer>>> arrays_sizes = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> ejecuciones = new ArrayList<>();
+        ArrayList<Integer> arrays = new ArrayList<>(); 
+
+            //creación de la estructura para guardar todos los arreglos
+        for(int i = 0; i < 5; i++) ejecuciones.add(arrays); //se agregan las 5 ejecuciones de para un tamaño n
+        for(int i = 0; i < 8; i++) arrays_sizes.add(ejecuciones); //se crean los ocho conjuntos de 5 ejecuciones para cada tamaño
+ 
+
+        //System.out.println(arrays_sizes.size()); //Prueba para detectar errores
+        //System.out.println(ejecuciones.size()); //Prueba para detectar errores
+
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 5; j++) { // Se llenan cada una de las 5 ejecuciones
+                for(int k = 0; k < sizes[i]; k++) { 
+                    arrays_sizes.get(i).get(j).add(random.nextInt(101)); //con el número correspondiente de valores
+                }
+            }
+        }
+
+        //arreglo de tres dimensiones para guardar los resultados
+        int[][][] results = new int[7][8][5]; //[algoritmo][tamaño del arreglo][#ejecución]
+
+        //0. BubbleSort 
+        for(int i = 0; i < 8; i++) { //tamaño
+            for(int j = 0; j < 5; j++) { //ejecución
+                int []aux = Utilerias.toArray(arrays_sizes.get(i).get(j)); //convierte el ArrayList a array (no objeto)
+                results[0][i][j] = BubbleSort.bubbleSort(aux, sizes[i]); //[algoritmo][tamaño del arreglo][#ejecución]
+            }
+        }
+        //1. InsertionSort
+        for(int i = 0; i < 8; i++) { //tamaño
+            for(int j = 0; j < 5; j++) { //ejecución
+                int []aux = Utilerias.toArray(arrays_sizes.get(i).get(j)); //convierte el ArrayList a array (no objeto)
+                results[1][i][j] = InsertionSort.insertionSort(aux, sizes[i]); //[algoritmo][tamaño del arreglo][#ejecución]
+            }
+        }
+        //2. SelectionSort
+        for(int i = 0; i < 8; i++) { //tamaño
+            for(int j = 0; j < 5; j++) { //ejecución
+                int []aux = Utilerias.toArray(arrays_sizes.get(i).get(j)); //convierte el ArrayList a array (no objeto)
+                results[2][i][j] = SelectionSort.selectionSort(aux, sizes[i]); //[algoritmo][tamaño del arreglo][#ejecución]
+            }
+        }
+        //3. HeapSort
+        for(int i = 0; i < 8; i++) { //tamaño
+            for(int j = 0; j < 5; j++) { //ejecución
+                int []aux = Utilerias.toArray(arrays_sizes.get(i).get(j)); //convierte el ArrayList a array (no objeto)
+                results[3][i][j] = HeapSort.heapSort(aux, sizes[i]); //[algoritmo][tamaño del arreglo][#ejecución]
+            }
+        }
+        //4. QuickSort
+        for(int i = 0; i < 8; i++) { //tamaño
+            for(int j = 0; j < 5; j++) { //ejecución
+                int []aux = Utilerias.toArray(arrays_sizes.get(i).get(j)); //convierte el ArrayList a array (no objeto)
+                results[4][i][j] = QuickSort.quickSort(aux, 0, sizes[i] - 1); //[algoritmo][tamaño del arreglo][#ejecución]
+            }
+        }
+        //5. MergeSort
+        for(int i = 0; i < 8; i++) { //tamaño
+            for(int j = 0; j < 5; j++) { //ejecución
+                int []aux = Utilerias.toArray(arrays_sizes.get(i).get(j)); //convierte el ArrayList a array (no objeto)
+                results[5][i][j] = MergeSort.mergeSort(aux, 0, sizes[i] - 1); //[algoritmo][tamaño del arreglo][#ejecución]
+            }
+        }
+        //6. PancakeSort
+        for(int i = 0; i < 8; i++) { //tamaño
+            for(int j = 0; j < 5; j++) { //ejecución
+                int []aux = Utilerias.toArray(arrays_sizes.get(i).get(j)); //convierte el ArrayList a array (no objeto)
+                results[6][i][j] = PancakeSort.pancakeSort(aux, sizes[i]); //[algoritmo][tamaño del arreglo][#ejecución]
+            }
+        }
+
+    //Impresión de resultados
+
+        System.out.println("Tabla de resultados bubble sort");
+        
+
+
+    //Obtención de promedios 
+        int size = 13, size2 = 100, cont1, cont2, cont3 ,cont4 ,cont5, cont6, cont7; 
 
         int[] a = {1, 4, 12, 31, 42, 11, 1, 66, 32, 8, 9, 19, 10}; //size = 13
         int[] b = {1, 4, 12, 31, 42, 11, 1, 66, 32, 8, 9, 19, 10};
         int[] c = {1, 4, 12, 31, 42, 11, 1, 66, 32, 8, 9, 19, 10};
         int[] d = {1, 4, 12, 31, 42, 11, 1, 66, 32, 8, 9, 19, 10};  
         int[] e = {1, 4, 12, 31, 42, 11, 1, 66, 32, 8, 9, 19, 10}; 
-        int[] f = {1, 4, 12, 31, 42, 11, 1, 66, 32, 8, 9, 19, 10}; 
+        int[] f = {1, 4, 12, 31, 42, 11, 1, 66, 32, 8, 9, 19, 10};
 
         //verificación
         cont1 = BubbleSort.bubbleSort(a, size); 
@@ -33,15 +126,15 @@ public class Project1 {
         System.out.println(cont4);
         System.out.println();
 
-        cont5 = QuickSort.quickSort(e, 0, size-1);
-        Utilerias.printArray(e, size);
+        cont5 = QuickSort.quickSort(d, 0, size-1);
+        Utilerias.printArray(d, size);
         System.out.println(cont5);
         System.out.println();
 
-        cont6 = PancakeSort.pancakeSort(f,size);
+        cont7 = MergeSort.mergeSort(f,0, size-1);
         Utilerias.printArray(f, size);
-        System.out.println(cont6);
+        System.out.println(cont7);
         System.out.println();
 
     }
-}   
+}
---
+++ Sort/MergeSort.java
@@ -0,0 +1,85 @@
+package Sort; 
+//import java.util.Random;
+
+public class MergeSort {
+    //static Random aleatorio = new Random(System.currentTimeMillis());
+    /*public static void main(String[] args) {
+        int[] a;
+        a = new int[20];   
+
+        for(int i = 0; i < 20; i++) a[i] = aleatorio.nextInt(100) + 1;
+        
+
+
+        mergeSort(a, 0, 19);
+        printArray(a);
+    }*/
+
+    public static int merge(int arr[], int left, int mid, int right) {
+        int cont = 0; 
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        //se crean dos sublistas
+        int L[] = new int[n1]; 
+        int R[] = new int[n2];
+
+
+        //se copian los valores de la lista original a la sublista 1
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[left + i];
+        //se copina los valores de la lista original a la sublista 2
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[mid + 1 + j];
+            
+        int i = 0, j = 0;
+
+        //se hace la combinación y se insertan los elementos en la lista original
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) { //comparación
+                arr[k] = L[i]; //inserción
+                i++;
+            } else {
+                arr[k] = R[j]; //inserción
+                j++;
+            }
+            k++;
+            cont += 2; 
+        }
+        //si ya se insertaron todos los elementos de la sublista derecha se insertan el resto de la lista izquierda
+        while (i < n1) {
+            arr[k] = L[i]; //inserción
+            i++;
+            k++;
+            cont++;
+        }
+        //si ya se insertaron todos los elementos de la sublista izquierdo se inserta el resto de la lista derecha
+        while (j < n2) {
+            arr[k] = R[j]; //inserción
+            j++;
+            k++;
+            cont++; 
+        }
+        return cont; 
+        //System.out.println("Iteración ");
+        //for(int p = left; p < right; p++) System.out.print(arr[p]+", "); 
+    }
+
+    public static int mergeSort(int arr[], int left, int right) {
+        int cont = 0; 
+
+        if (left < right) { //comparación
+            cont ++; 
+            int mid = (left + right) / 2;
+
+            cont += mergeSort(arr, left, mid);
+            cont += mergeSort(arr, mid + 1, right);
+
+            cont += merge(arr, left, mid, right);
+        }
+
+        return cont; 
+    }
+}
